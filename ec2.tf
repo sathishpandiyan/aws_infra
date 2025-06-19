55b3b0a9f584695b758b4a1d1cb55023f793aa21
sathishpandiyan <sathishpandiyan1993@gmail.com>
2025-06-19 14:25:42 +0530

    modfied

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "example" {
   ami                    = "ami-09e6f87a47903347c"  # Replace with your desired AMI ID
   instance_type          = "t2.micro"              # Replace with your desired instance type
   subnet_id              = "subnet-0f149193d192149f1" # Replace with your subnet ID
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  #vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name = "th"  # Replace with your key pair name
 
   tags = {
